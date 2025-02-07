eadf56489ab81922b28e387cb9d9e03e2d110f44
asherlie <lieber.asher@gmail.com>
2025-02-07 12:36:04 -0500

    added search functionality

--- srch.c
+++ srch.c
@@ -1,34 +1,32 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
  * Display > Screen > Windows
  */
 
-void search_windows(Display* d, Screen* scr) {
+void search_windows(Display* d, Screen* scr, char* sterm) {
     Window rw, parent;
     Window* children;
     int nchildren;
 
     XQueryTree(d, RootWindowOfScreen(scr), &rw, &parent, &children, &nchildren);
 
-    printf("%i total windows\n", nchildren);
-
     char* wn;
     for (int i = 0; i < nchildren; ++i) {
         XFetchName(d, children[i], &wn);
-        if (wn) {
+        if (wn && (!sterm || strcasestr(wn, sterm))) {
             printf("\"%s\"\n", wn);
         }
     }
 }
 
-int main() {
+int main(int argc, char** argv) {
     Display *d;
     Screen* scr;
     int s;
 
-    printf("Attempting to open %s\n", XDisplayName(NULL));
 	d = XOpenDisplay(NULL);
     if (!d) {
         printf("Failed to open %s\n", XDisplayName(NULL));
@@ -36,5 +34,5 @@ int main() {
     s = DefaultScreen(d);
     scr = ScreenOfDisplay(d, s);
 
-    search_windows(d, scr);
+    search_windows(d, scr, argc == 1 ? NULL : argv[1]);
 }
