fe079237ddf1d5b2bc8792bce249eacb660447a8
asherlie <lieber.asher@gmail.com>
2025-02-07 19:02:32 -0500

    window locator now flashes red around the window border

--- srch.c
+++ srch.c
@@ -1,10 +1,20 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 
 /*
  * Display > Screen > Windows
  */
+
+unsigned long get_rgb_ul (uint8_t r, uint8_t g, uint8_t b) {
+    unsigned long ret = (r << 16) | (g << 8) | b;
+    return ret;
+}
+
+void locate_window(Display* d, Window* w, unsigned long color, uint16_t flash_duration, uint8_t n_secs) {
+    XSetWindowBorder(d, w, color, );
+}
 
 void search_windows(Display* d, Screen* scr, char* sterm) {
     Window rw, parent;
@@ -17,6 +27,10 @@ void search_windows(Display* d, Screen* scr, char* sterm) {
     for (int i = 0; i < nchildren; ++i) {
         XFetchName(d, children[i], &wn);
         if (wn && (!sterm || strcasestr(wn, sterm))) {
+            Pixmap p;
+            if (sterm) {
+                locate_window(d, children[i], get_rgb_ul(255, 0, 0));
+            }
             printf("\"%s\"\n", wn);
         }
     }
